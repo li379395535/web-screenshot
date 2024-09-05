44d2292bda5be50840a9ef1ccf4978a64efd932c
lichenfeng <379395535@QQ.com>
2024-09-05 16:22:58 +0800

    fix: img load issue

--- src/lib/split-methods/drawCrossImg.ts
+++ src/lib/split-methods/drawCrossImg.ts
@@ -6,24 +6,30 @@ export function drawCrossImg(html: Document) {
     if (!href) return;
     if (href && href.startsWith("base64")) return;
     const promise = new Promise((resolve, reject) => {
-      const img = new Image();
-      img.crossOrigin = "anonymous";
-      img.src = `${href}&time=${+new Date().getTime()}`;
-      img.onload = function() {
-        const width = element.width;
-        const height = element.height;
-        const canvas = document.createElement("canvas");
-        canvas.width = width;
-        canvas.height = height;
-        const ctx: any = canvas.getContext("2d");
-        ctx.drawImage(img, 0, 0, width, height);
-        const base64 = canvas?.toDataURL();
-        element.setAttribute("src", base64);
-        resolve("转换成功");
-      };
-      img.onerror = reject;
-      if (href !== null) {
-        img.src = href;
+      try {
+        const img = new Image();
+        img.crossOrigin = "anonymous";
+        img.src = `${href}&time=${+new Date().getTime()}`;
+        img.onload = function() {
+          const width = element.width;
+          const height = element.height;
+          const canvas = document.createElement("canvas");
+          canvas.width = width;
+          canvas.height = height;
+          const ctx: any = canvas.getContext("2d");
+          ctx.drawImage(img, 0, 0, width, height);
+          const base64 = canvas?.toDataURL();
+          element.setAttribute("src", base64);
+          resolve("转换成功");
+        };
+        img.onerror = () => {
+          resolve("转换失败");
+        };
+        if (href !== null) {
+          img.src = href;
+        }
+      } catch (error) {
+        console.warn(error);
       }
     });
     promises.push(promise as Promise<string>);
